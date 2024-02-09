4c5707695ddbb65b55f00e57cd411b9d5834735b
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-02-09 23:55:40 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 364 ms, Memory: 153344 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -4,7 +4,17 @@ input = sys.stdin.readline
 s = input()
 q = int(input())
 
+cnt = [[0 for _ in range(len(s)+1)] for _ in range(26)]
+
+# 입력받은 문자열에 대한 누적합 초기화
+for i in range(len(s)):
+    for j in range(26):
+        if ord(s[i]) - 97 == j: # ord(s[i]) - 97 > a~z를 0~25로 표현
+            cnt[j][i+1] = cnt[j][i] + 1
+        else: 
+            cnt[j][i+1] = cnt[j][i]
+
 for _ in range(q):
-    alpha, l, r = map(str, input().split())
-    findT = s[int(l):int(r)+1]
-    print(findT.count(alpha))
+    alpha, l, r = input().split()
+    answer = cnt[ord(alpha)-97][int(r)+1] - cnt[ord(alpha)-97][int(l)]
+    print(answer)
