c76e7daa7ff8c4695ce995d3203804184151a10d
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-03-14 00:01:08 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+tree = {}
+
+for _ in range(n):
+    root, left, right = input().split()
+    tree[root] = [left, right]
+
+def preorder(root):
+    if root != '.':
+        print(root, end='') # root
+        preorder(tree[root][0]) # left
+        preorder(tree[root][1]) # right
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0]) # left
+        print(root, end='') # root
+        inorder(tree[root][1]) # right
+
+def postorder(root): 
+    if root != '.':
+        postorder(tree[root][0]) # left
+        postorder(tree[root][1]) # right
+        print(root, end='')
+
+preorder("A")
+print()
+inorder("A")
+print()
+postorder("A")
