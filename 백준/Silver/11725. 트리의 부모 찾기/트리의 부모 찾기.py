67253d6e461ae11f7d94ddfb1ed9abc7707fa662
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-03-17 15:50:25 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 348 ms, Memory: 49760 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,5 +1,3 @@
-# 11725 트리의 부모 찾기
-
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -13,11 +11,16 @@ for _ in range(n-1):
     nodes[a].append(b)
     nodes[b].append(a)
 
-def dfs(v):
-    for i in nodes[v]:
-        if visited[i] == 0:
-            visited[i] = v
-            dfs(i)
-dfs(1)
-for i in range(2, n+1):
+queue = deque()
+queue.append(1)
+def bfs():
+    while queue:
+        now = queue.popleft()
+        for i in nodes[now]:
+            if visited[i] == 0:
+                visited[i] = now
+                queue.append(i)
+bfs()
+
+for i in range(2,n+1):
     print(visited[i])
