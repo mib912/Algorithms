e0a049a441bf31aaf3709f7b9ee5d11ba6fda991
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-04-24 00:08:34 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -4,14 +4,8 @@ input = sys.stdin.readline
 n = int(input())
 cnt = 0
 
-
-for a in range(2,n):
-    for b in range(1,n):
-        for c in range(1,n):
-            if c -b < 2:
-                continue
-            if a % 2 == 1:
-                continue
-            if a + b + c == n:
-                cnt += 1
+# n 개중 a에게 줄 짝수개 i를 빼고 c에게 줄 2개를 뺀 수에서 b,c가 나눠갖는다.
+for i in range(2, n-1, 2): # 영훈이에게 줄 사탕 1개 빼기
+    candy = n-i-2 # 남규에게 줄 사탕 최소 2개 빼기
+    cnt += candy //2
 print(cnt)
