960d9cd7c4dd973ced3b0dbebe869d4c46b83df4
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-04-27 23:55:53 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+cnt = 0
+
+while n >= 2 and m >= 1 and n+m >= k+3:
+    n -= 2
+    m -= 1
+    cnt += 1
+print(cnt)
