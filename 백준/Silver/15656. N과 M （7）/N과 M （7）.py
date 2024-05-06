e92b7c496c8ef81e7bb63397e09168641d7e7cb2
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-05-06 17:17:56 +0900

    [Silver III] Title: N과 M (7), Time: 2252 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+selected = [0] * m
+def recur(cur):
+    if cur == m:
+        print(*selected)
+        return
+    for i in range(n):
+        selected[cur] = arr[i]
+        recur(cur+1)
+
+recur(0)
