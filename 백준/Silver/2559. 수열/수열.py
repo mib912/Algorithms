630de878a57023a0d578d51137baf73cff011656
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-06-02 00:26:37 +0900

    [Silver III] Title: 수열, Time: 88 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -2,11 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-temps = list(map(int, input().split()))
+ls = list(map(int, input().split()))
 
-tot = sum(temps[:k])
-answer = tot
-for i in range(k, n):
-    tot += temps[i] - temps[i-k]
-    answer = max(answer, tot)
-print(answer)
+partSum = sum(ls[:k])
+result = [partSum]
+
+for i in range(n-k):
+    partSum = partSum + ls[i+k] - ls[i] 
+    result.append(partSum)
+
+print(max(result))
