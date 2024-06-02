02e24c62dab5b57d778674770ca6dcbfaed4d04c
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-06-02 10:15:28 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1076 ms, Memory: 105940 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,15 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
 
-sumArr = [[0]*(n+1) for _ in range(n+1)]
+ls = [list(map(int, input().split())) for _ in range(n)]
+prefixSum = [[0]*(n+1) for _ in range(n+1)]
+
 # 누적합
-for i in range(1,n+1):
+for i in range(1, n+1):
     for j in range(1, n+1):
-        sumArr[i][j] = sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1] + arr[i-1][j-1]
+        prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1] + ls[i-1][j-1]
 
 for _ in range(m):
-    x,y,a,b = map(int, input().split()) # (x,y)부터 (a,b)까지의 부분합
-    partSum = sumArr[a][b] - sumArr[x-1][b] - sumArr[a][y-1] + sumArr[x-1][y-1]
+    x1, y1, x2, y2 = map(int, input().split()) #(x1,y1)~(x2,y2) 부분합
+    partSum = prefixSum[x2][y2] - prefixSum[x1-1][y2] - prefixSum[x2][y1-1] + prefixSum[x1-1][y1-1]
     print(partSum)
