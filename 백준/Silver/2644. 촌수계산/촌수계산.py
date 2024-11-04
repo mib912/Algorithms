e100b5f5840893ee9502fb660660935404d1c34a
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-11-04 23:28:45 +0900

    [Silver II] Title: 촌수계산, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,30 +1,30 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
 
 n = int(input())
 a, b = map(int, input().split())
 m = int(input())
+
 graph = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
 flag = False # 촌수 찾았는지 확인
 
 for _ in range(m):
-    x, y = map(int, input().split())
+    x,y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
-def dfs(v, count):
+def dfs(v, cnt):
     global flag
     visited[v] = True
 
     if v == b:
         flag = True
-        print(count)
+        print(cnt)
 
     for i in graph[v]:
-        if not visited[i]:
-            dfs(i, count+1)
+        if visited[i] == False:
+            dfs(i, cnt + 1)    
 
 dfs(a, 0)
 if flag == False:
