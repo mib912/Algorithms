bd03a81ad460f22aedfacc56ed60e7e2d00f90b1
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2024-11-11 00:16:37 +0900

    [Silver V] Title: 거스름돈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+answer = 0
+
+
+while n > 0:
+    if n % 5 == 0:
+        answer += n // 5
+        break
+    n -= 2
+    answer += 1
+
+if n < 0:
+    print(-1)
+else:
+    print(answer) 
