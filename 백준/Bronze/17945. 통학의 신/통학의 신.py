845b4cd1c05e4bfc1ae5f8ffef9a33e19330da8b
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2025-02-02 22:54:18 +0900

    [Bronze III] Title: 통학의 신, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.py
+++ 백준/Bronze/17945. 통학의 신/통학의 신.py
@@ -1,10 +1,8 @@
 import sys
-
 input = sys.stdin.readline
 
 a,b = map(int, input().split())
-answer = set()
+
 for i in range(-1000,1001):
-    if i**2 + 2*a*i + b == 0:
-        answer.add(i)
-print(*sorted(answer))
+    if (i**2 + 2*a*i + b) == 0:
+        print(i, end=' ')
