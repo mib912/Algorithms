4cbe77b53d1ec1339aadb0d62f1923d270812499
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2025-04-05 00:37:07 +0900

    [Silver III] Title: 수열, Time: 72 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -2,13 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-ls = list(map(int, input().split()))
 
-partSum = sum(ls[:k])
+temps = list(map(int, input().split()))
+
+partSum = sum(temps[:k])
 result = [partSum]
 
-for i in range(n-k):
-    partSum = partSum + ls[i+k] - ls[i] 
-    result.append(partSum)
+for i in range(n-k): # n개의 수를  k씩 연속합을 구하면 나오는 갯수
+    result.append(result[i]-temps[i]+temps[i+k])
 
 print(max(result))
