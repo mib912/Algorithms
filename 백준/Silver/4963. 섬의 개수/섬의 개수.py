6e03820759a20f45852a99ef87b6a9f1a12b56f2
MINJUNG KANG <81951233+mib912@users.noreply.github.com>
2025-04-07 21:22:42 +0900

    [Silver II] Title: 섬의 개수, Time: 60 ms, Memory: 32744 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,32 +1,31 @@
 import sys
-from collections import deque
-
-input = sys.stdin.readline
 sys.setrecursionlimit(10**7)
 
-# 상하좌우 대각선
-dx = [-1,1,0,0,1,1,-1,-1]
-dy = [0,0,-1,1,1,-1,1,-1]
+input = sys.stdin.readline
+
+dx = [1,-1,0,0,1,1,-1,-1]
+dy = [0,0,1,-1,1,-1,1,-1]
 
 def dfs(x,y):
     if x < 0 or x >= h or y < 0 or y >= w:
         return
+    
     if graph[x][y] == 1:
-        graph[x][y] = 0 # 방문처리
+        graph[x][y] = 0  # 방문처리
+
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
-            dfs(nx, ny)
+            dfs(nx,ny)
 
-result = []
 while True:
+    answer = 0
     w, h = map(int, input().split())
     if w == 0 and h == 0:
         break
-
+    
     graph = [list(map(int, input().split())) for _ in range(h)]
-    answer = 0
-
+    
     for i in range(h):
         for j in range(w):
             if graph[i][j] == 1:
